3b29334287e2f7ddc9c64d709a2147621ec5bf98
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-21 14:33:07 -0700

    dry out method code. Remove unnecessary empty variables.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,17 +2,13 @@ class String
   define_method(:anagram)  do
     downcase_words = self.downcase
     word_array = downcase_words.split(",").to_a
-    word1 = (word_array[0]).to_s.split("")
-    word2 = (word_array[1]).to_s.split("")
-
-    palindrone_outcome = ""
-    anagram_outcome = ""
-    not_a_word = ""
-
-    word_test = word1.join("") + word2.join("")
+    word1 = (word_array[0]).split("")
+    word2 = (word_array[1]).split("")
 
     sorted_word1 = word1.sort()
     sorted_word2 = word2.sort()
+
+    word_test = word1.join("") + word2.join("")
 
 
     if word_test.scan(/[aeiou]/).count > 0
@@ -27,8 +23,6 @@ class String
     else
       not_a_word = "not a word. Please input a word."
     end
-
-
 
   "This is #{not_a_word}#{anagram_outcome}#{palindrone_outcome}"
   end
