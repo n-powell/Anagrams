693986269aa29f715fdf181bad18ef09d1cfd0b1
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-21 16:34:33 -0700

    add two more integration specs to ensure output is changing with new intigration input.

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -11,4 +11,17 @@ describe('the anagram path', {:type => :feature}) do
     click_button('Submit!')
     expect(page).to have_content('Anagram Results This is an Anagram.')
   end
+
+  it ('Takes user input number and returns coin results.') do
+    visit('/')
+    fill_in('words', :with => 'elvis,sivle')
+    click_button('Submit!')
+    expect(page).to have_content('Anagram Results This is an Anagram. This is also a Palindrome.')
+  end
+  it ('Takes user input number and returns coin results.') do
+    visit('/')
+    fill_in('words', :with => 'ttttss,ttttss')
+    click_button('Submit!')
+    expect(page).to have_content("This is not a word. Please input a word.")
+  end
 end
