354a6cd82c1d1bfd3c9942a42088314f42c054a9
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-21 14:29:09 -0700

    add vowel tester to take user input and determine if words or not by testing if input contains vowels.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,19 +7,29 @@ class String
 
     palindrone_outcome = ""
     anagram_outcome = ""
+    not_a_word = ""
+
+    word_test = word1.join("") + word2.join("")
 
     sorted_word1 = word1.sort()
     sorted_word2 = word2.sort()
 
-    if (sorted_word1.join("") == sorted_word2.join(""))
-      anagram_outcome = "an Anagram"
-      if (word1.join("") == word2.join("").reverse())
-        palindrone_outcome = " This is also a Palindrome."
+
+    if word_test.scan(/[aeiou]/).count > 0
+      if (sorted_word1.join("") == sorted_word2.join(""))
+        anagram_outcome = "an Anagram."
+        if (word1.join("") == word2.join("").reverse())
+          palindrone_outcome = " This is also a Palindrome."
+        end
+      elsif (sorted_word1.join("") != sorted_word2.join(""))
+        anagram_outcome = "Not an Anagram."
       end
-    elsif (sorted_word1.join("") != sorted_word2.join(""))
-      anagram_outcome = "Not an Anagram"
+    else
+      not_a_word = "not a word. Please input a word."
     end
 
-  "This is #{anagram_outcome}.#{palindrone_outcome}"
+
+
+  "This is #{not_a_word}#{anagram_outcome}#{palindrone_outcome}"
   end
 end
