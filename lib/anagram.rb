65264e0818a3d848b21256fe8e036515383e8705
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-21 13:17:47 -0700

    add more spec tests for possible outcomes of palindrome.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,14 @@ class String
     word1 = (word_array[0]).to_s.split("")
     word2 = (word_array[1]).to_s.split("")
 
+    palindrone_outcome = ""
+
+    if (word1.join("") == word2.join("").reverse())
+      palindrone_outcome = "a Palindrome"
+    elsif (word1.join("") != word2.join("").reverse())
+      palindrone_outcome = "Not a Palindrome"
+    end
+
 
     anagram_outcome = ""
 
@@ -12,11 +20,11 @@ class String
     word2.sort!()
 
     if (word1.join("") == word2.join(""))
-      anagram_outcome = "Anagram"
+      anagram_outcome = "an Anagram"
     elsif (word1.join("") != word2.join(""))
       anagram_outcome = "Not an Anagram"
     end
 
-  anagram_outcome
+  "This is #{palindrone_outcome}. This is #{anagram_outcome}"
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,34 +3,43 @@ require ('anagram.rb')
 
 describe('String#anagram') do
   it('tests to see if the user input is an anagram') do
-    expect("elvis,lives".anagram()).to(eq("Anagram"))
+    expect("elvis,lives".anagram()).to(eq("This is Not a Palindrome. This is an Anagram"))
   end
 
   it('tests to see if the user input is an anagram') do
-    expect("elvis,livasdes".anagram()).to(eq("Not an Anagram"))
+    expect("elvis,livasdes".anagram()).to(eq("This is Not a Palindrome. This is Not an Anagram"))
   end
 
   it('tests to see if the user input is an anagram') do
-    expect("words,test".anagram()).to(eq("Not an Anagram"))
+    expect("words,test".anagram()).to(eq("This is Not a Palindrome. This is Not an Anagram"))
   end
 
   it('tests to see if the user input is an anagram') do
-    expect("setter,tester".anagram()).to(eq("Anagram"))
+    expect("setter,tester".anagram()).to(eq("This is Not a Palindrome. This is an Anagram"))
   end
 
   it('tests to see if the user input is an anagram, regardless of Uppercase letters') do
-    expect("Setter,Tester".anagram()).to(eq("Anagram"))
+    expect("Setter,Tester".anagram()).to(eq("This is Not a Palindrome. This is an Anagram"))
   end
 
   it('tests to see if the user input is an anagram, regardless of Uppercase letters') do
-    expect("SetTer,TeSter".anagram()).to(eq("Anagram"))
+    expect("SetTer,TeSter".anagram()).to(eq("This is Not a Palindrome. This is an Anagram"))
   end
 
   it('tests to see if the user input is an anagram, regardless of Uppercase letters') do
-    expect("TAP,Pat".anagram()).to(eq("Anagram"))
+    expect("TAP,Pat".anagram()).to(eq("This is a Palindrome. This is an Anagram"))
   end
 
   it('tests to see if the user input is a Palandrome') do
-    expect("test,tset".anagram()).to(eq("Palandrome"))
+    expect("test,tset".anagram()).to(eq("This is a Palindrome. This is an Anagram"))
   end
+
+  it('tests to see if the user input is a Palandrome') do
+    expect("TERRUT,TURRET".anagram()).to(eq("This is a Palindrome. This is an Anagram"))
+  end
+
+  it('tests to see if the user input is a Palandrome') do
+    expect("Not,going to be either".anagram()).to(eq("This is Not a Palindrome. This is Not an Anagram"))
+  end
+
 end
