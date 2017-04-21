5d242a6562685cd0072b77116616b800571f2c35
Nick Powell <nickpowell@Nicks-iMac.local>
2017-04-21 12:29:00 -0700

    add three more specs to test functionality of correct output of anagram and not anagram.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,16 @@ describe('String#anagram') do
   it('tests to see if the user input is an anagram') do
     expect("elvis,lives".anagram()).to(eq("Anagram"))
   end
+
+  it('tests to see if the user input is an anagram') do
+    expect("elvis,livasdes".anagram()).to(eq("Not an Anagram"))
+  end
+
+  it('tests to see if the user input is an anagram') do
+    expect("words,test".anagram()).to(eq("Not an Anagram"))
+  end
+
+  it('tests to see if the user input is an anagram') do
+    expect("setter,tester".anagram()).to(eq("Anagram"))
+  end
 end
